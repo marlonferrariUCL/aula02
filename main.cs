c6e037ebabc9582577a69b7ff34f7cbf44329a32
Marlon Ferrari <marlonferrari@ucl.br>
2020-08-19 00:31:17 +0000

    ex01 OK

--- main.cs
+++ main.cs
@@ -1,9 +1,19 @@
 using System;
 
 class MainClass {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-    Console.WriteLine ("Hello World");
-    Console.WriteLine ("Hello World");
+
+  public static void ExibeNumero (float numero) {
+    Console.WriteLine (" O número informado foi {0}", numero);
+    }
+
+  //COORDENAÇÃO
+  public static void Main () {
+    float numero;
+    Console.Write("Digite um número >> ");
+    //TYPECAST
+    numero = float.Parse(Console.ReadLine());
+    ExibeNumero(numero);
+    
   }
+
 }
